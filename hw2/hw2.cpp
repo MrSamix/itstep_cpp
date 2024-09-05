ba16eae3e373d89a41dc14ec445d37dfe5751ae1
Oleksandr Panchuk <alekssen2008@gmail.com>
2024-09-05 20:23:30 +0300

    Added hw2

---
+++ hw2/hw2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I have many pencils:\nRed and green and blue.\nI can draw a picture\nAnd give it to you.\nI can draw my mother,\nI can draw my father,\nI can draw my sister,\nAnd my little brother.";
+    cout << "Every" << endl;
+    cout << "\tHunter" << endl;
+    cout << "\t\tWants" << endl;
+    cout << "\t\t\tTo know" << endl;
+    cout << "\t\t\t\t Where" << endl;
+    cout << "\t\t\t\t\tDoes Pheasant" << endl;
+    cout << "\t\t\t\t\t\t\tSit" << endl;
+
+    cout << "Selling puppies." << endl;
+    cout << "Breed: Rottweiler." << endl;
+    cout << "..." << endl;
+    cout << "Tel.: 222-22-22" << endl;
+    cout << "222";
+    cout << "\t| 222 |" << endl;
+    cout << "22";
+    cout << "\t| 22 |" << endl;
+    cout << "22";
+    cout << "\t| 22 |" << endl;
+}
