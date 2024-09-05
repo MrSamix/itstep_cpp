22ad3956a58b2e20a928b437fd77206c9b583190
Oleksandr Panchuk <alekssen2008@gmail.com>
2024-09-05 19:58:12 +0300

    Added hw1

---
+++ hw1/My first C++ project/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	/* Task 5 */
+	cout << "Hello, World!" << endl;
+	cout << "/Brajne Stroustrup/" << endl;
+	
+	/* Task 6 */
+	cout << "Hello, World!" << endl;
+	cout << "\\\\Dennis Ritchi\\\\" << endl;
+
+	/* Task 7 */
+	cout << "To be, or not to be: that is the question:\nWhether 'tis nobler in the mind to suffer\nThe slings and arrows of outrageous fortune,\nOr to take arms against a sea of troubles,\nAnd by opposing end them ?\nWilliam Shakespeare" << endl;
+}
