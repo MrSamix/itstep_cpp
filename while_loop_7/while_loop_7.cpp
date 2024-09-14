ada3d1de65ef951f546d980fa3d8fa94b911feab
Oleksandr Panchuk <alekssen2008@gmail.com>
2024-09-14 14:03:05 +0300

    added hw7(fully)

--- while_loop_7/while_loop_7.cpp
+++ while_loop_7/while_loop_7.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	// hw 1
 	//// Task 1
- //   int a;
+	//int a;
 	//cout << "Enter a number: ";
 	//cin >> a;
 	//int counter = a;
@@ -96,12 +96,104 @@ int main()
 		int digit1 = num2 / 100;
 		int digit2 = (num2 / 10) % 10;
 		int digit3 = num2 % 10;
-		num1++;
+		num2++;
 
-		if (digit1 != digit2 || digit1 != digit3 || digit2 != digit3)
+		if (digit1 != digit2 && digit1 != digit3 && digit2 != digit3)
 		{
 			count2++;
 		}
 	}
-	cout << "The number of integers with two identical digits is: " << count2 << endl;
+	cout << "The number of integers with two unidentical digits is: " << count2 << endl;
+
+	// Task 3
+	int number;
+	cout << "Enter an integer task3 : ";
+	cin >> number;
+	
+	int result = 0;
+	int multiplier = 1;
+	
+	while (number != 0)
+	{
+	    int digit = number % 10;
+	    if (digit != 3 && digit != 6)
+	    {
+	        result += digit * multiplier;
+	        multiplier *= 10;
+	    }
+	    number /= 10;
+	}
+	
+	cout << "The modified number is: " << result << endl;
+
+	// Task 4
+	int A;
+	cout << "Enter an integer task4 : ";
+	cin >> A;
+	
+	int B = 1;
+	while (B <= A)
+	{
+	    if (A % (B * B) == 0 && A % (B * B * B) != 0)
+	    {
+	        cout << B << endl;
+	    }
+	    B++;
+	}
+
+	// Task 5
+	int a;
+	cout << "Enter an integer task5 : ";
+	cin >> a;
+	
+	int sum = 0;
+	int number5 = a;
+	while (number5 != 0)
+	{
+	    int digit = number5 % 10;
+	    sum += digit;
+	    number5 /= 10;
+	}
+	
+	int cube = sum * sum * sum;
+	
+	if (cube == a * a)
+	{
+	    cout << "The cube of the sum of the digits is equal to A * A." << endl;
+	}
+	else
+	{
+	    cout << "The cube of the sum of the digits is not equal to A * A." << endl;
+	}
+
+	// Task 6
+	int number6;
+	cout << "Enter an integer task6 : ";
+	cin >> number6;
+	
+	int divisor = 1; // дільник
+	while (divisor <= number6)
+	{
+	    if (number6 % divisor == 0)
+	    {
+	        cout << divisor << endl;
+	    }
+	    divisor++;
+	}
+
+	// Task 7
+	int number7_1, number7_2;
+	cout << "Enter an integers: ";
+	cin >> number7_1 >> number7_2;
+
+	divisor = 1; // дільник
+	int min = number7_1 < number7_2 ? number7_1 : number7_2;
+	while (divisor <= min)
+	{
+		if (number7_1 % divisor == 0 && number7_2 % divisor == 0)
+		{
+			cout << divisor << endl;
+		}
+		divisor++;
+	}
 }
