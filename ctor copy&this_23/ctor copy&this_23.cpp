19bfa548ee6f6512195e1170300f5d8e12e319ba
Oleksandr Panchuk <alekssen2008@gmail.com>
2024-11-05 22:27:24 +0200

    added hw23

--- ctor copy&this_23/ctor copy&this_23.cpp
+++ ctor copy&this_23/ctor copy&this_23.cpp
@@ -4,15 +4,20 @@
 using namespace std;
 
 int main() {
-    String str1("sdqcg");
-    String str2("rgfas34");
+    String str1("hello");
+    String str2("world");
+    String str3(80);
 
+    String res = str1 * str2;
 
-    String result = str1 * str2;
+    cout << "After mult str1 and str2: ";
+    res.print();
 
-    cout << "After mult str1 and str2: " << endl;
-    result.print();
-
-    str2.input();
-    str2.print();
+    str3.input();
+    str3.print();
+    string res_str3 = str3.getStr();
+    size_t capacity_str3 = str3.getLength();
+    cout << "Str: " << res_str3 << "\tCapacity = " << capacity_str3 << endl;
+    str3.setStr("Hello world!");
+    str3.print();
 }
